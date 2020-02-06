fff5db94512cddd12229d9da2315b090b9a74ca0
Filip Ditrich <ditrich@visionarygraphics.cz>
2020-02-07 00:17:13 +0100

    functional error boudary?

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -17,38 +17,46 @@ export const Home: React.FC = () => {
 	};
 	
 	return (
-		<ErrorHandler>
-			<div className="button-container">
-				<button onClick={ () => toastIt(toast.TYPE.DEFAULT) } className="button button-primary">
-					<span>Primary</span></button>
-				<button onClick={ () => toastIt(toast.TYPE.DEFAULT) } className="button button-primary button-reverse">
-					<span>Primary Reverse</span></button>
-				<button onClick={ () => toastIt(toast.TYPE.DEFAULT) } className="button button-secondary">
-					<span>Secondary</span></button>
-				<button onClick={ () => toastIt(toast.TYPE.DEFAULT) }
-				        className="button button-secondary button-reverse"><span>Secondary Reverse</span></button>
-			</div>
-			<div className="button-container">
-				<button onClick={ () => toastIt(toast.TYPE.ERROR) } className="button button-danger"><span>Danger</span>
-				</button>
-				<button onClick={ () => toastIt(toast.TYPE.ERROR) } className="button button-danger button-reverse">
-					<span>Danger Reverse</span></button>
-				<button onClick={ () => toastIt(toast.TYPE.WARNING) } className="button button-warning">
-					<span>Warning</span></button>
-				<button onClick={ () => toastIt(toast.TYPE.WARNING) } className="button button-warning button-reverse">
-					<span>Warning Reverse</span></button>
-			</div>
-			<div className="button-container">
-				<button onClick={ () => toastIt(toast.TYPE.INFO) } className="button button-info"><span>Info</span>
-				</button>
-				<button onClick={ () => toastIt(toast.TYPE.INFO) } className="button button-info button-reverse"><span>Info Reverse</span>
-				</button>
-				<button onClick={ () => toastIt(toast.TYPE.SUCCESS) } className="button button-success">
-					<span>Success</span></button>
-				<button onClick={ () => toastIt(toast.TYPE.SUCCESS) } className="button button-success button-reverse">
-					<span>Success Reverse</span></button>
-			</div>
-		</ErrorHandler>
+		<div>
+			<ErrorHandler>
+				<div className="button-container">
+					<button onClick={ () => toastIt(toast.TYPE.DEFAULT) } className="button button-primary">
+						<span>Primary</span></button>
+					<button onClick={ () => toastIt(toast.TYPE.DEFAULT) }
+					        className="button button-primary button-reverse">
+						<span>Primary Reverse</span></button>
+					<button onClick={ () => toastIt(toast.TYPE.DEFAULT) } className="button button-secondary">
+						<span>Secondary</span></button>
+					<button onClick={ () => toastIt(toast.TYPE.DEFAULT) }
+					        className="button button-secondary button-reverse"><span>Secondary Reverse</span></button>
+				</div>
+				<div className="button-container">
+					<button onClick={ () => {
+						throw new Error("wrong button dick");
+					} } className="button button-danger"><span>Danger</span>
+					</button>
+					<button onClick={ () => toastIt(toast.TYPE.ERROR) } className="button button-danger button-reverse">
+						<span>Danger Reverse</span></button>
+					<button onClick={ () => toastIt(toast.TYPE.WARNING) } className="button button-warning">
+						<span>Warning</span></button>
+					<button onClick={ () => toastIt(toast.TYPE.WARNING) }
+					        className="button button-warning button-reverse">
+						<span>Warning Reverse</span></button>
+				</div>
+				<div className="button-container">
+					<button onClick={ () => toastIt(toast.TYPE.INFO) } className="button button-info"><span>Info</span>
+					</button>
+					<button onClick={ () => toastIt(toast.TYPE.INFO) } className="button button-info button-reverse">
+						<span>Info Reverse</span>
+					</button>
+					<button onClick={ () => toastIt(toast.TYPE.SUCCESS) } className="button button-success">
+						<span>Success</span></button>
+					<button onClick={ () => toastIt(toast.TYPE.SUCCESS) }
+					        className="button button-success button-reverse">
+						<span>Success Reverse</span></button>
+				</div>
+			</ErrorHandler>
+		</div>
 	);
 };
 
